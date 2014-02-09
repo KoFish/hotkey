ee4ad37a767bf96a516dd9d787fcf5fe67c43e89
Calvin <calvin@bastille>
2014-02-09 01:17:16 -0500

    sucks formatting

--- keys.h
+++ keys.h
@@ -1,6 +1,5 @@
 static const Key hotkeys[] = {
-	//"Command				 MOD 											Key
-	{ "xterm &", 				 Mod1Mask, XK_g },
+	{ "xterm &", Mod1Mask, XK_g },
 	{ "xterm -e mail &", ShiftMask|ControlMask, XK_m },
 	{ "chromium &", Mod1Mask, XK_z }
 };
